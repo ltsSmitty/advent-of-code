f11031bf7db255cef88bb06aa57c194d2c72c1d7
Smitty Penman <smitty.penman@gmail.com>
2023-12-12 08:53:53 -0500

    middle of some revisions

--- 11/main.go
+++ 11/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"math"
 	"os"
 	"time"
 )
@@ -57,36 +58,114 @@ func Part1(data []string) int {
 		expanded = append(expanded, line)
 		}
 	}
-
-	log.Printf("Expanded rows")
-	for _, line := range expanded {
-		log.Printf("%v", line)
-	}
-
-	// expand each column
-	// if column is empty, expand to 2 columns
 	columnsExpanded := []string{}
 	columnsExpanded = append(columnsExpanded, expanded...)
 
 	for i :=  len(expanded[0])-1; i >= 0; i-- {
 		if ColumnIsEmpty(expanded, i) {
-			fmt.Printf("empty column %d\n", i)
+			// fmt.Printf("empty column %d\n", i)
 			columnsExpanded = InsertRuneAtIndex(columnsExpanded, i, '.')		
 		}
 	}
 	
+	starMap := MakeStarMap(columnsExpanded)
+	log.Printf("%v", starMap)
 
-	log.Printf("Expanded columns")
-	for _, line := range columnsExpanded {
-		log.Printf("%v", line)
+	totalDistance:=0
+	counter := 1
+	for star1 :=0; star1 < len(starMap); star1++ {
+		log.Printf("star %v", star1)		
+		for star2 := star1+1; star2 < len(starMap); star2++ {
+			log.Printf("star1: %v, star2: %v", starMap[star1], starMap[star2])
+			distance := math.Abs(float64(starMap[star1].X - starMap[star2].X)) + math.Abs(float64(starMap[star1].Y - starMap[star2].Y))
+			log.Printf("distance: %v", distance)
+			totalDistance += int(distance)
+			counter++
+			log.Printf("Loops %v", counter)
+		}		
 	}
-
-	return 999
+	return totalDistance
 }
 
-// func Part2(data []string) int {
-// 	return 999
-// }
+func Part2(data []string) int {
+	starMap := MakeStarMap(data)
+	log.Printf("%v", starMap)
+
+	totalDistance:=0
+	DISTANCE_MULPLIER := 2
+
+	for star1 :=0; star1 < len(starMap); star1++ {
+		log.Printf("star %v", star1)		
+		for star2 := star1+1; star2 < len(starMap); star2++ {
+			currentLocation := Coord{starMap[star2].X, starMap[star2].Y}
+			distanceMoved := 0
+
+			for currentX := starMap[star2].X; startingX != starMap[star1].X; startingX++ {
+				// move in the x direction
+				if starMap[star1].X < starMap[star2].X {
+					// move right
+					for currentLocation.X < starMap[star1].X {
+						currentLocation.X++
+						if ColumnIsEmpty(data, currentLocation.X) {
+							distanceMoved += 1*DISTANCE_MULPLIER
+						} else {
+							distanceMoved += 1
+						}
+					}
+				} else {
+					// move left
+					for currentLocation.X > starMap[star1].X {
+						currentLocation.X--
+						if ColumnIsEmpty(data, currentLocation.X) {
+							distanceMoved += 1*DISTANCE_MULPLIER
+						} else {
+							distanceMoved += 1
+						}
+					}
+				}
+			} 
+			for startingY := starMap[star2].Y; startingY != starMap[star1].Y; startingY++ {
+				// move in the y direction
+				if starMap[star1].Y < starMap[star2].Y {
+					// move down
+					for currentLocation.Y < starMap[star1].Y {
+						currentLocation.Y++
+						if RowIsEmpty(data[currentLocation.Y]) {
+							distanceMoved += 1*DISTANCE_MULPLIER
+						} else {
+							distanceMoved += 1
+						}
+					}
+				} else {
+					// move up
+					for currentLocation.Y > starMap[star1].Y {
+						currentLocation.Y--
+						if RowIsEmpty(data[currentLocation.Y]) {
+							distanceMoved += 1*DISTANCE_MULPLIER
+						} else {
+							distanceMoved += 1
+						}
+					}
+				}
+			} else {
+				// same star
+				distanceMoved = 0
+			}
+			
+
+			// log.Printf("star1: %v, star2: %v", starMap[star1], starMap[star2])
+			// distance := math.Abs(float64(starMap[star1].X - starMap[star2].X)) + math.Abs(float64(starMap[star1].Y - starMap[star2].Y))
+			// log.Printf("distance: %v", distance)
+			// totalDistance += int(distance)
+			// counter++
+			// log.Printf("Loops %v", counter)
+		}		
+	}
+	return totalDistance
+
+
+	return 0
+}
 
 func main() {
 	startTime := time.Now()
@@ -124,3 +203,31 @@ func InsertRuneAtIndex(data []string, index int, r rune) []string {
 	}
 	return data
 }
+
+type Coord struct {
+	X int
+	Y int
+}
+
+
+func MakeStarMap (data []string) []Coord {
+	starMap := []Coord{}
+	for y, row := range data {
+		for x, char := range row {
+			if char == '#' {
+				starMap = append(starMap, Coord{x, y})
+			}
+		}
+	}
+	return starMap
+}
+
+/*
+
+	start from the original map
+	step toward the next star
+		start by moving in an x position
+		check if the new x is in an empty column. 
+			if so, distanceMoved +=1e6
+			otherwise distanceMoved +=1	
+*/
