f992b076c40d65631ab79552778fb317ee7fcd25
Smitty Penman <smitty.penman@gmail.com>
2023-12-13 12:54:24 -0500

    stumped on part 2

--- 12/main.go
+++ 12/main.go
@@ -16,18 +16,6 @@ func check(e error) {
 	}
 }
 
-// one line
-// func LoadInput(fileName string) []string {
-// 	input := []string{}
-
-// 	file, err := os.ReadFile(fileName)
-// 	check(err)
-// 	for _, data := range string(file) {
-// 		input = append(input, string(data))
-// 	}
-// 	return input
-// }
-
 // multiple lines
 func LoadInput(fileName string) []string {
 	input := []string{}
@@ -45,24 +33,47 @@ func LoadInput(fileName string) []string {
 	return input
 }
 
+var instructionMap InstructionMap = make(map[CodeInstruction]int)
+
 func Part1(data []string) int {
-	return 999
+	counter := 0
+	multiplier:= 1
+
+	for _, row := range data {
+		counter += RunRow(row, multiplier)
+	}
+	return counter
 }
 
-// func Part2(data []string) int {
-// 	return 999
-// }
+type CodeInstruction struct {
+	code string
+	instructionsFlat int
+}
+
+type InstructionMap map[CodeInstruction]int
+
+
+func Part2(data []string) int {
+	counter := 0
+	multiplier:= 5
+	for i, row := range data {
+		log.Printf("Row :%d",i)
+		counter += RunRow(row, multiplier)
+	}
+	return counter
+}
+
 
 func main() {
 	startTime := time.Now()
 	data := LoadInput("input.txt")
-	fmt.Printf("Part 1: %v\n", Part1(data))
-	fmt.Println(time.Since(startTime))
-	// fmt.Printf("Part 2: %v\n", Part2(data))
+	// fmt.Printf("Part 1: %v\n", Part1(data))
 	// fmt.Println(time.Since(startTime))
+	fmt.Printf("Part 2: %v\n", Part2(data))
+	fmt.Println(time.Since(startTime))
 }
 
-func RunRow (row string) int {
+func RunRow (row string, multiplier int ) int {
 	r := strings.Split(row, " ")
 	code := r[0]
 	instructions := strings.Split(r[1], ",")
@@ -72,101 +83,168 @@ func RunRow (row string) int {
 		num, _ := strconv.Atoi(i)
 		numInstructions = append(numInstructions, num)
 	}
-	// log.Printf("Code: %v\n Instructions: %v", code, numInstructions)
+	log.Printf("Original Code: %v  Instructions: %v", code, numInstructions)
 
-	answer := AnalyzeString(code, numInstructions)
+	multipleCode := code
+	multipleInstructions := numInstructions
+	for i:=1; i<multiplier; i++ {
+		multipleCode = multipleCode + "?" + code
+		multipleInstructions = append(multipleInstructions, numInstructions...)
+	}
+
+	log.Printf("Multiple Code: %v  Instructions: %v", multipleCode, multipleInstructions)
+
+	answer := AnalyzeString(multipleCode, multipleInstructions)
 	log.Printf("Answer: %v", answer)
 	return answer
 }
 
-func AnalyzeString (code string, instructions []int) int {
-	counter :=0
+func AnalyzeString (code string, instructions []int ) int {
+	// log.Printf("Code: %v  Instructions: %v", code, instructions)
 
-	numSymbolsToProcess:=0
+	if val := LoadFromMap(code, instructions); val != -1 {
+		// log.Printf("Found [%v : %v] in map",code, instructions)
+		return val
+	}
+
+	// numSymbolsToProcess:= len(instructions)-1
+	numSymbolsToProcess:= 0
 	for _, i := range instructions {
 		numSymbolsToProcess += i
 	}
 	if len(code) < numSymbolsToProcess {
-		log.Printf(`Impossible to finish - Not enough code to process %v symbols`, numSymbolsToProcess)
-		return 0
-	
+		log.Printf("Saving false - not enough symbols to process. Code: %v  Instructions: %v ", code, instructions)
+		SaveToMap(code, instructions, 0)
+		return 0	
+	}
+
+	if len(instructions) == 0 {
+		if strings.Count(code, "#") > 0 {
+			log.Printf("Saving false - out of instructions but more #s remain. Code: %v  Instructions: %v ", code, instructions)
+			SaveToMap(code, instructions, 0)
+			return 0
+		}
+		log.Printf("Saving true - no more #s left. Code: %v  Instructions: %v ", code, instructions)
+		SaveToMap(code, instructions, 1)
+		return 1
 	}
 
 	if (len(code) == 0) {
 		if len(instructions) == 0 {
-			log.Printf(`out of code and instructions - should be a win!`)
+			log.Printf("Saving true- code and instructions are empty. Code: %v  Instructions: %v", code, instructions)
+			SaveToMap(code, instructions, 1)
 			return 1
-		} else {
-			log.Printf(`out of code, but still have instructions left: %v`, instructions)
+			} else {
+			log.Printf("Saving false - code empty but instructions remain. Code: %v  Instructions: %v", code, instructions)
+			SaveToMap(code, instructions, 0)
 			return 0
 		}
 	}
-	log.Printf(`Current data: %v - %v`, code, instructions)
 	if string(code[0]) == "." {
-		counter += AnalyzeString(code[1:], instructions)
+		return AnalyzeString(code[1:], instructions)
 	}
-	if (string(code[0]) == "?") {
-		log.Printf(`At a '?', so running with both '.' and '#'`)
-		counter += AnalyzeString(string("." +code[1:]), instructions)
-		counter += AnalyzeString(string("#" +code[1:]), instructions)
-	}
-	if (string(code[0]) == "#") {
-		log.Printf(`is #`)
-		if DoesStartWithNHashes(code, instructions[0]) {
-			// remove the first n chars from code and the 0th instruction
-			// if the next character is a ?, turn it into a .
-			// otherwise keep removing #s until its ? or . 
-				// and if it is a ?, turn it into a .
-			log.Printf(`Code %v does start with at least %v hashes`, code, instructions[0])
-			s := code[(instructions[0]):]
-			log.Printf(`stripped off from the instructions, s is now %v`, s)
-			// if len(code) > instructions[0] {
-				Subloop:
-				for i, char := range s {
-					log.Printf(`Checking char %v (%v)`, i, string(char))
-					switch string(char) {
-						case ".":
-						case "?":
-							// todo this isnt quite working
-							s = "." + s[i+1:]
-							log.Printf("There's a ? or . at %v, so attempting to continue/break (%v)", i,s)
-							break Subloop
-						case "#":
-							log.Printf("There's a # at %v (%v), so continuing", i,string(char))
-						}
-					// if string(char) != "#" {
-					// 	s = "." + code[(instructions[0]+1):]
-					// 	continue
-					// } else {
-					// 	s += string(char)
-					// }
-				}
 
-			// } else {
-			// 	log.Printf(`Code %v is too short to remove %v chars`, code, instructions[0])
-			// 	s = code[(instructions[0]):]
-			// }
-			// do i need to check if the next is a ? and turn it into a . to create a new chunk?
-			return counter + AnalyzeString(s, instructions[1:])
-			
-		} else {
-			log.Printf(`Code %v doesn't start with %v hashes`, code, instructions[0])
-		}		
+	if (string(code[0]) == "?") {
+		value := AnalyzeString(string(code[1:]), instructions) + AnalyzeString(string("#" +code[1:]), instructions)
+		log.Printf("Saving in ? after branch. Code: %v  Instructions: %v  Value: %v", code, instructions, value)
+		SaveToMap(code, instructions, value)
+		return value
 	}
-	return counter
+
+	if (string(code[0]) == "#") {
+		if DoesStartWithNHashes(code, instructions[0]) {
+			s := code[(instructions[0]):]
+			trimmedInstructions := instructions[1:]
+
+			if len(s) == 0 {
+				v := IsAtGoodEndpoint(s, trimmedInstructions)
+				log.Printf("Saving at terminus. Code: %v  Instructions: %v  Value: %v", code, instructions, v)
+				SaveToMap(code, instructions, v)
+				return v
+			}
+
+			switch (string(s[0])) {
+				case "?":
+					s = "." + s[1:]
+				case "#":
+					for string(s[0]) == "#" {
+						s = s[1:]
+					}
+					if string(s[0]) == "?" {
+						s = "." + s[1:]
+					}
+				// default aka '.', do nothing
+			}
+			// do i need to save here
+			val:= AnalyzeString(s, trimmedInstructions)	
+			log.Printf("Saving in #. Code: %v  Instructions: %v  Value: %v", code, instructions, val)
+			return val	
+		} else {
+			log.Printf("Saving false - code starts with # but wrong amount for instructions. Code: %v  Instructions: %v", code, instructions)
+			SaveToMap(code, instructions, 0)
+			return 0
+		
+		}
+	}
+	return 0
 }
 
 func DoesStartWithNHashes(s string, n int) bool {
-	log.Printf(`Trying to see if %v starts with %v hashes`, s, n)
 	counter := 0
+	isSuccess := false
 	for _, char := range s {
 		if string(char) == "." {
 			return false
 		}
 		counter++
 		if counter == n {
+			isSuccess = true
+			break
+		}
+	}
+	// make sure the next character isn't a #
+	if isSuccess {
+		if len(s) == counter {
+			return true
+		}
+		if string(s[counter]) == "#" {
+			return false
+		} else {
 			return true
 		}
 	}
 	return false
+}
+
+func IsAtGoodEndpoint (code string, instructions []int) int {
+	// log.Printf(`Checking that %v is empty`,instructions)
+	if len(code) == 0 && len(instructions) == 0 {
+		// log.Printf(`It is!`)
+		return 1
+	}
+	// log.Printf(`It isn't!`)
+	return 0
+}
+
+func compressIntSliceIntoInt (instructions []int) int {
+	// log.Printf(`Compressing %v into an int`, instructions)
+	counter := 0
+	for _, i := range instructions {
+		counter = counter*10 + i
+	}
+	return counter
+}
+
+func SaveToMap(code string, instructions []int, value int) {
+	num := compressIntSliceIntoInt(instructions)
+	instructionMap[CodeInstruction{code, num}] = value
+}
+
+func LoadFromMap(code string, instructions []int) int {
+	num := compressIntSliceIntoInt(instructions)
+	ci := CodeInstruction{code, num}
+	if val, ok := (instructionMap)[ci]; ok {
+		return val
+	}
+	return -1
 }
--- 12/main_test.go
+++ 12/main_test.go
@@ -10,20 +10,31 @@ func TestLoadInput(t *testing.T) {
 	assert.Equal(t, 1, 1)
 }
 
-func TestPart1(t *testing.T) {
-	assert.Equal(t, 21, Part1(LoadInput("input_test.txt")))
-}
+// func TestRunRow(t *testing.T) {
+// 	assert.Equal(t, 1, RunRow("???.### 1,1,3",1))
+// 	assert.Equal(t, 4, RunRow(".??..??...?##. 1,1,3",1))
+// 	assert.Equal(t, 1, RunRow("?#?#?#?#?#?#?#? 1,3,1,6",1))
+// 	assert.Equal(t, 1, RunRow("????.#...#... 4,1,1",1))
+// 	assert.Equal(t, 4, RunRow("????.######..#####. 1,6,5",1))
+// 	assert.Equal(t, 10, RunRow("?###???????? 3,2,1",1))
 
-func TestRunRow(t *testing.T) {
-	// assert.Equal(t, 1, RunRow("???.### 1,1,3"))
-	// assert.Equal(t, 4, RunRow(".??..??...?##. 1,1,3"))
-	assert.Equal(t, 1, RunRow("?#?#?#?#?#?#?#? 1,3,1,6"))
-	// assert.Equal(t, 1, RunRow("????.#...#... 4,1,1"))
-	// assert.Equal(t, 4, RunRow("????.######..#####. 1,6,5"))
-	// assert.Equal(t, 10, RunRow("?###???????? 3,2,1"))
-
-}
+// }
 
 func TestRunRow1(t *testing.T) {
-	assert.Equal(t, 2, RunRow("???.### 1,1,3"))
+	assert.Equal(t, 2, RunRow("???.### 1,1,3",1))
+}
+
+func TestPart1(t *testing.T) {
+	assert.Equal(t, 20, Part1(LoadInput("input_test.txt")))
+}
+
+func TestRunRowMultiplier(t *testing.T){
+	// assert.Equal(t, 2, RunRow(".# 1",5))
+	val := RunRow("??.??????????.????.??????????.????.??????????.????.??????????.????.??????????.? 1,1,3,1,1,1,1,3,1,1,1,1,3,1,1,1,1,3,1,1,1,1,3,1,1",1)
+	assert.Equal(t, 1, val)
+	// assert.Equal(t, 15, RunRow("????.#...#... 4,1,1",5))
+}
+
+func TestRunPart2(t *testing.T) {
+	assert.Equal(t, 525151, Part2(LoadInput("input_test.txt")))
 }
